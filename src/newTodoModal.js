8b892269f71f08d1907cddf27cb1573c9450bdac
Alan Patrick <alan.patrick.fv@gmail.com>
2022-05-22 13:38:20 -0300

    Set style for newTodoModal inputs

--- src/newTodoModal.js
+++ src/newTodoModal.js
@@ -6,16 +6,21 @@ export default function newTodoModal () {
 
     // Title
     const titleInput = document.createElement("input");
+    titleInput.classList.add('ntm-title');
+    titleInput.placeholder = "Title..."
     container.appendChild(titleInput);
 
     // Due Date
     const dueToInput = document.createElement("input");
+    dueToInput.classList.add('ntm-date');
     dueToInput.type = "date";
     container.appendChild(dueToInput);
     
     // Description
     const descInput = document.createElement('input');
+    descInput.classList.add('ntm-description');
     descInput.type = "textarea";
+    descInput.placeholder = "Description..."
     container.appendChild(descInput);
 
     return container;
