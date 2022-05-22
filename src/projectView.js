6a39b25c931d6f64ae7b33c2fbed64b38fd0bf92
Alan Patrick <alan.patrick.fv@gmail.com>
2022-05-22 11:22:17 -0300

    Initialize addBtn subcomponent of projectViewer

--- src/projectView.js
+++ src/projectView.js
@@ -3,17 +3,25 @@ import todoComponent from "./todoComponent";
 export default function projectViewer (proj) {
     const container = document.createElement('div');
     container.classList.add("pjview-container")
-    
+
+    // Project Title
     const title = container.appendChild(document.createElement('h2'));
     title.classList.add('pjview-title');
     title.textContent = proj.getId();
 
+    // Project Todo List
     const todoBucket = document.createElement("div");
     todoBucket.classList.add("pjview-bucket");
     for (const item of proj.getEveryEntry()) {
         todoBucket.appendChild(todoComponent(item[1]));
     }
     container.appendChild(todoBucket);
-    
+
+    // Project Add Todo
+    const  addTodo = document.createElement("button");
+    addTodo.classList.add("pjview-addBtn");
+    container.appendChild(addTodo);
+    addTodo.textContent = "+";
+
     return container
 }
