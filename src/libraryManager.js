2f325424abf1f143c70673b3168325aa2ddb5f5b
Alan Patrick <alan.patrick.fv@gmail.com>
2022-05-22 20:48:45 -0300

    Initialize newProjectModal

--- src/libraryManager.js
+++ src/libraryManager.js
@@ -1,4 +1,8 @@
+import newModalProj from './newProjModal';
+import projectViewer from './projectViewer';
 import style from './stylesheets/libraryManager.css';
+
+const root = document.querySelector("#app");
 
 export default function libraryManager (library) {
     const container = document.createElement('div');
@@ -9,6 +13,10 @@ export default function libraryManager (library) {
     container.appendChild(newProj);
     newProj.classList.add('libmgr-newProjBtn');
     newProj.textContent = "New Project";
+    newProj.addEventListener('click', e => {
+        container.remove();
+        root.appendChild(newModalProj());
+    });
 
     // Project List
     const projList = document.createElement('div');
@@ -16,6 +24,12 @@ export default function libraryManager (library) {
     for (const proj of library.getEveryEntry()) {
         const projTitle = document.createElement('h4');
         projTitle.textContent = proj[0];
+        projTitle.addEventListener('click', e => {
+            document.querySelector(".pjview-container").remove();
+            const newViewer = projectViewer(library.getProj(proj[0]));
+            root.appendChild(newViewer);
+            container.remove();
+        });
         projList.appendChild(projTitle);
     }
     container.appendChild(projList);
